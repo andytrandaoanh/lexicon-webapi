a34043bba93b61d9c5c8031ccae5eada1d68499e
andytrandaoanh <andytrandaoanh@gmail.com>
2019-08-06 23:45:37 +0700

    improved JSON process

--- handle_mongodb.py
+++ handle_mongodb.py
@@ -20,23 +20,84 @@ def fetchWordDetail(mongo, word):
 
 
 
+def processMeaning(meaning):
+	#pprint(meaning)
+	meaningList = []
+	for key in meaning:
+		objMeaning = {}
+		objMeaning['category'] = key
+		tempList = meaning[key]
+		newList = []
+		for item in tempList:
+			tempObj = {}
+			for k in item:
+				if k == 'synonyms':
+					syn_str = ', '.join(item[k])
+					tempObj['synonyms'] = syn_str
+				else:
+					tempObj[k] = item[k]
+
+			newList.append(tempObj)
+
+		objMeaning['meaning'] = newList
+		meaningList.append(objMeaning)
+	#for item in meaningList:
+	#	pprint(item)
+	return meaningList
+
+def processDoc(doc):
+	#pprint(doc)
+	wordList = []
+	wordHeader = {}
+	senseList = []
+	for key in doc:
+		if key == 'meaning':
+			objMeaning = doc['meaning']
+			senseList += processMeaning(objMeaning)
+		else:
+			wordHeader[key] = doc[key]
+
+	#print('word header:', wordHeader)
+	#print('sense list:', senseList)
+	for sense in senseList:
+		wordObj = {}
+		wordObj['category'] = sense['category']
+		wordObj['meaning'] = sense['meaning']
+		for key in wordHeader:
+			wordObj[key] = wordHeader[key]
+			wordList.append(wordObj)
+	#pprint(wordList)
+
+	return wordList
+
+def unpackData(data):
+	#purporse: flaten deeply-nested JSON data
+	docList = []
+	for doc in data:
+		newDoc = {}
+		for key in doc:
+			if key !=  '_id':
+				newDoc[key] = doc[key]
+		docList.append(newDoc)	
+	
+	dataOut = []
+	
+	for doc in docList:
+		#pprint(doc)
+		dataOut += processDoc(doc)
+	#return None
+	return dataOut
+
+
+
 
 def fetchGoogleDefinition(mongo, word):
 	if (word):
 		volumnName = 'google'
 		collection = mongo.db[volumnName]
 		data = collection.find({'word':word})
-		output = []
-		for doc in data:
-			newDoc = {}
-			for key in doc:
-				#print('key', key)
-				if key !=  '_id':
-					newDoc[key] = doc[key]
-			output.append(newDoc)
-			#output.append(doc)
-
-		#pprint(output)
+		output = unpackData(data)
+		#pprint(data)
 		resp = jsonify(output)
 		resp.status_code = 200
 		return  resp
